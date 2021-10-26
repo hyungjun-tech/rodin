092f3433c590c349ee7a355b61ab76fa1d4f4b50
hyungjun-tech <hyungjun@sindoh.com>
2021-10-26 16:31:39 +0900

    충돌 테스트 2

--- rodinP/GitTest.cpp
+++ rodinP/GitTest.cpp
@@ -2,5 +2,12 @@
 #include <GitTest.h>
 // This  File is Test
 int GitTest() {
-	return 0  ;
+	for (int i = 0; i < 100; i++) {
+		std::cout << i << std::endl;
+		if (i >= 50)
+			std::cout << "i is over 50 : " << i << std::endl;
+		else
+			std::cout << "i is under 50 : " << i << std::endl;
+	}
+	return 0;
 }
